cffae3ecb84a8ef560e812df22e8c7bed29c53dc
anais-codefresh <anais.urlichs@codefresh.io>
2020-12-01 10:37:55 +0100

    Set timeout

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,26 @@
 "use strict";
 var rookout = require('rookout');
-rookout.start();
 var express = require("express");
+var { createHttpTerminator } = require('http-terminator')
+
+rookout.start({
+    labels:
+        {
+            "env":"dev", // Optional,see Labels page below Projects
+        },
+    git_origin:"git@github.com:anais-codefresh/rookout-sample-app.git"
+});
 var app = express();
+const server = app.listen(5000);
+const httpTerminator = createHttpTerminator({ server });
+
 app.get('/', function (req, res) { return res.send("Hello World"); });
 app.get("/hello/:name", function (req, res) {
     res.send("Hello, " + req.params.name);
 });
-app.listen(5000);
+
+setTimeout(() => {
+    httpTerminator.terminate()
+}, 15000)
+  
 //# sourceMappingURL=app.js.map
